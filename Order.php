d87db92324b6885ef5249143dd8963177e7f760b
Vitek <vitekh@i.ua>
2016-02-11 22:13:59 +0200

    ware2 and order1

--- Order.php
+++ Order.php
@@ -2,16 +2,15 @@
 /**
 * 
 */
-class Warehouse
+class Order
 {
 	public $id;
 	public $x;
 	public $y;
+	public $count;
 	public $goods;
-
-
 	function __construct($id)
 	{
-		$this->id=$id;
+				$this->id = $id;
 	}
 }
